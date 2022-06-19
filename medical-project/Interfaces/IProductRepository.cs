c7ba76127828c7015fb449224d4261ba3d114a43
Abinab Dangi <aviii2057@gmail.com>
2022-06-19 19:56:25 +0545

    added product repo

---
+++ medical-project/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using medical_project.Dtos;
+using medical_project.Models;
+
+namespace medical_project.Interfaces
+{
+    public interface IProductRepository
+    {
+        void Update(Product product);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<ProductDto>> GetAllProductAsync();
+        Task<ProductDto> GetAllProductByIdAsync(int Id);
+        Task<IEnumerable<ProductDto>> GetProductByStoreId(int Id);
+
+        Task<bool> ProductExists(string name);
+
+    }
+}
